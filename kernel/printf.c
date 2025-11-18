f601cc3118cb1a16ba2e8a20fc2fd5c8088cc60b
yjx <14017018+yaojingxi666@user.noreply.gitee.com>
2025-11-18 15:47:44 +0800

    完成backtrace调用

--- kernel/printf.c
+++ kernel/printf.c
@@ -18,7 +18,8 @@
 volatile int panicked = 0;
 
 // lock to avoid interleaving concurrent printf's.
-static struct {
+static struct
+{
   struct spinlock lock;
   int locking;
 } pr;
@@ -32,20 +33,21 @@ printint(int xx, int base, int sign)
   int i;
   uint x;
 
-  if(sign && (sign = xx < 0))
+  if (sign && (sign = xx < 0))
     x = -xx;
   else
     x = xx;
 
   i = 0;
-  do {
+  do
+  {
     buf[i++] = digits[x % base];
-  } while((x /= base) != 0);
+  } while ((x /= base) != 0);
 
-  if(sign)
+  if (sign)
     buf[i++] = '-';
 
-  while(--i >= 0)
+  while (--i >= 0)
     consputc(buf[i]);
 }
 
@@ -60,30 +62,32 @@ printptr(uint64 x)
 }
 
 // Print to the console. only understands %d, %x, %p, %s.
-void
-printf(char *fmt, ...)
+void printf(char *fmt, ...)
 {
   va_list ap;
   int i, c, locking;
   char *s;
 
   locking = pr.locking;
-  if(locking)
+  if (locking)
     acquire(&pr.lock);
 
   if (fmt == 0)
     panic("null fmt");
 
   va_start(ap, fmt);
-  for(i = 0; (c = fmt[i] & 0xff) != 0; i++){
-    if(c != '%'){
+  for (i = 0; (c = fmt[i] & 0xff) != 0; i++)
+  {
+    if (c != '%')
+    {
       consputc(c);
       continue;
     }
     c = fmt[++i] & 0xff;
-    if(c == 0)
+    if (c == 0)
       break;
-    switch(c){
+    switch (c)
+    {
     case 'd':
       printint(va_arg(ap, int), 10, 1);
       break;
@@ -94,9 +98,9 @@ printf(char *fmt, ...)
       printptr(va_arg(ap, uint64));
       break;
     case 's':
-      if((s = va_arg(ap, char*)) == 0)
+      if ((s = va_arg(ap, char *)) == 0)
         s = "(null)";
-      for(; *s; s++)
+      for (; *s; s++)
         consputc(*s);
       break;
     case '%':
@@ -111,25 +115,44 @@ printf(char *fmt, ...)
   }
   va_end(ap);
 
-  if(locking)
+  if (locking)
     release(&pr.lock);
 }
 
-void
-panic(char *s)
+void panic(char *s)
 {
   pr.locking = 0;
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();//给panic添加回溯功能
   panicked = 1; // freeze uart output from other CPUs
-  for(;;)
+  for (;;)
     ;
 }
 
-void
-printfinit(void)
+void printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace()
+{
+  printf("backtrace:\n");
+  //获取当前栈的栈指针
+  uint64 fp = r_fp();
+  //进行递归到出口处
+  while(fp != 0 ){
+    //获取上一层的地址
+    uint64 ret = *(uint64*)(fp - 8);
+    printf("%p\n",ret);
+    //开始进行递归
+    uint64 pre_fp = *(uint64*)(fp - 16);
+    //如果pre_fp和当前的fp不是在同一个page就退出循环
+    if(PGROUNDDOWN(pre_fp) != PGROUNDDOWN(fp))break;
+    fp = pre_fp;//递归
+  }
+  
+}
+
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -53,7 +53,7 @@ sys_sleep(void)
 {
   int n;
   uint ticks0;
-
+  
   argint(0, &n);
   if(n < 0)
     n = 0;
@@ -67,6 +67,7 @@ sys_sleep(void)
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+  backtrace();
   return 0;
 }
 
